15daa0af11a4ad5f3c3f276096a8ecf7f2366a31
smartwalle <smartwalle@gmail.com>
2019-01-31 13:19:23 +0800

    添加基于 redis stream 的分布式日志收集&输出功能

--- sample/redis/sub.go
+++ sample/redis/sub.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	var rw = log4go.NewRedisHub("youle_log", "192.168.1.99:6379", 10, 2)
-	rw.Redirect(log4go.NewFileWriter(log4go.K_LOG_LEVEL_TRACE, log4go.WithLogDir("./yl")))
+	rw.Redirect(log4go.NewFileWriter(log4go.K_LOG_LEVEL_TRACE, log4go.WithLogDir("./test_log")))
 
 	fmt.Println("running")
 	select {}
