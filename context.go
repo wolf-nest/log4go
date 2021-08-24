4a12c7033b70484c94cd77a581bac2c33e237a50
smartwalle <smartwalle@gmail.com>
2021-08-24 17:44:18 +0800

    log id

--- context.go
+++ context.go
@@ -20,6 +20,17 @@ func NewContext(ctx context.Context) context.Context {
 	return ctx
 }
 
+func WithId(ctx context.Context, logId string) context.Context {
+	if ctx == nil {
+		ctx = context.TODO()
+	}
+	logId = strings.TrimSpace(logId)
+	if logId == "" {
+		logId = uuid.NewString()
+	}
+	return context.WithValue(ctx, logIdKey{}, logId)
+}
+
 func MustGetId(ctx context.Context) string {
 	var logId = GetId(ctx)
 	if logId == "" {
@@ -34,15 +45,4 @@ func GetId(ctx context.Context) string {
 	}
 	var id, _ = ctx.Value(logIdKey{}).(string)
 	return id
-}
-
-func WithId(ctx context.Context, logId string) context.Context {
-	if ctx == nil {
-		ctx = context.TODO()
-	}
-	logId = strings.TrimSpace(logId)
-	if logId == "" {
-		logId = uuid.NewString()
-	}
-	return context.WithValue(ctx, logIdKey{}, logId)
 }
