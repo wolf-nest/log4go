399f0600171dd8588e246cb64154161dbfcfedaa
smartwalle <smartwalle@gmail.com>
2021-08-24 17:52:20 +0800

    log id

--- log_test.go
+++ log_test.go
@@ -1,6 +1,7 @@
 package log4go_test
 
 import (
+	"context"
 	"github.com/smartwalle/log4go"
 	"os"
 	"testing"
@@ -14,20 +15,21 @@ func TestMain(m *testing.M) {
 }
 
 func TestLogger_Write(t *testing.T) {
-	log4go.Traceln("default logger trace", 1)
-	log4go.Tracef("default logger trace fmt %d \n", 10)
-	log4go.Debugln("default logger debug", 1)
-	log4go.Debugf("default logger debug fmt %d \n", 10)
-	log4go.Infoln("default logger info", 1)
-	log4go.Infof("default logger info fmt %d \n", 10)
-	log4go.Warnln("default logger warn", 1)
-	log4go.Warnf("default logger warn fmt %d \n", 10)
-	log4go.Errorln("default logger error", 1)
-	log4go.Errorf("default logger error fmt %d \n", 10)
+	log4go.Traceln(nil,"default logger trace", 1)
+	log4go.Tracef(nil,"default logger trace fmt %d \n", 10)
+	log4go.Debugln(nil,"default logger debug", 1)
+	log4go.Debugf(nil,"default logger debug fmt %d \n", 10)
+	log4go.Infoln(nil,"default logger info", 1)
+	log4go.Infof(nil,"default logger info fmt %d \n", 10)
+	log4go.Warnln(nil,"default logger warn", 1)
+	log4go.Warnf(nil,"default logger warn fmt %d \n", 10)
+	log4go.Errorln(nil,"default logger error", 1)
+	log4go.Errorf(nil,"default logger error fmt %d \n", 10)
 }
 
 func BenchmarkPrintln(b *testing.B) {
+	var ctx = log4go.NewContext(context.Background())
 	for i := 0; i < b.N; i++ {
-		log4go.Println("1", "2", "3", "4", "5")
+		log4go.Println(ctx,"1", "2", "3", "4", "5")
 	}
 }
